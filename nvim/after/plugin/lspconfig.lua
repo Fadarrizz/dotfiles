845e2cbe0eefb77c8782932fab0479841d0b8748
Fadarrizz <auk.geerts@gmail.com>
2023-05-05 10:25:37 +0200

    Update lsp config

--- nvim/after/plugin/lspconfig.lua
+++ nvim/after/plugin/lspconfig.lua
@@ -1,101 +1,100 @@
--- IMPORTANT: make sure to setup neodev BEFORE lspconfig
-require("neodev").setup({
-    library = { plugins = { "neotest" }, types = true },
-})
+local ih = require('inlay-hints')
+ih.setup()
 
-local lsp = require('lsp-zero')
+local lsp = require('lsp-zero').preset('recommended')
 
-lsp.preset('recommended')
+lsp.on_attach(function(client, bufnr)
+  lsp.default_keymaps({ buffer = bufnr })
 
-lsp.ensure_installed({
-    'html',
-    'eslint',
-    'rust_analyzer',
-    'tailwindcss',
-    'intelephense',
-})
-
-lsp.nvim_workspace()
-
-local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
-local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-  ["<C-Space>"] = cmp.mapping.complete(),
-})
-
-lsp.setup_nvim_cmp({
-  mapping = cmp_mappings
-})
-
--- See: https://github.com/neovim/nvim-lspconfig/blob/master/lua/lspconfig/server_configurations/intelephense.lua
-lsp.configure('intelephense', {
-})
-
--- Configure LSP through rust-tools.nvim plugin.
--- rust-tools will configure and enable certain LSP features for us.
--- See https://github.com/simrat39/rust-tools.nvim#configuration
-lsp.configure('rust_analyzer', {
-    tools = {
-        runnables = {
-          use_telescope = true,
-        },
-        inlay_hints = {
-          auto = true,
-          show_parameter_hints = true,
-          parameter_hints_prefix = "<- ",
-          other_hints_prefix = "=> ",
-        },
-    },
-    -- these override the defaults set by rust-tools.nvim
-    -- see https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md#rust_analyzer
-    server = {
-        settings = {
-          -- to enable rust-analyzer settings visit:
-          -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
-          ["rust-analyzer"] = {
-            -- enable clippy on save
-            checkOnSave = {
-              command = "clippy",
-            },
-          },
-        },
-    }
-})
-
-lsp.configure('html', {
-    filetypes = {'html', 'blade', 'handlebars'}
-})
-
-lsp.on_attach(function (client, bufnr)
   local opts = { noremap=true, silent=true, buffer=bufnr }
-
   vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
-  vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-  vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-  vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
-  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-  vim.keymap.set('n', '<space>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, opts)
-  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
   vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, opts)
-  -- vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-  vim.keymap.set('n', 'gr', ':Telescope lsp_references<CR>', opts)
   vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, opts)
 end)
 
+lsp.ensure_installed({
+  'html',
+  'eslint',
+  'rust_analyzer',
+  'tailwindcss',
+  'intelephense',
+})
+
+require('lspconfig').lua_ls.setup({
+  on_attach = function(client, bufnr)
+    ih.on_attach(client, bufnr)
+  end,
+  settings = {
+    Lua = {
+      hint = {
+        enable = true,
+      },
+    },
+  },
+})
+
+-- See: https://github.com/neovim/nvim-lspconfig/blob/master/lua/lspconfig/server_configurations/intelephense.lua
+lsp.configure('intelephense', {})
+
+lsp.skip_server_setup({ 'rust_analyzer' })
+
+lsp.configure('html', {
+  filetypes = { 'html', 'blade', 'handlebars' }
+})
+
+-- lsp.on_attach(function (client, bufnr)
+--   local opts = { noremap=true, silent=true, buffer=bufnr }
+
+--   vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+--   vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+--   vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+--   vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+--   vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+--   vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+--   vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+--   vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+--   vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+--   vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+--   vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+--   vim.keymap.set('n', '<space>wl', function()
+--     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+--   end, opts)
+--   vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+--   vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+--   vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, opts)
+--   -- vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+--   vim.keymap.set('n', 'gr', ':Telescope lsp_references<CR>', opts)
+--   vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, opts)
+-- end)
+
 lsp.setup()
 
-vim.diagnostic.config({
-    virtual_text = true
+-- CMP
+local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+local cmp = require('cmp')
+cmp.event:on(
+  'confirm_done',
+  cmp_autopairs.on_confirm_done()
+)
+-- END CMP
+
+-- Rust
+local rust_tools = require('rust-tools')
+
+rust_tools.setup({
+  tools = {
+    on_initialized = function()
+      ih.set_all()
+    end,
+    inlay_hints = {
+      auto = false,
+    },
+  },
+  server = {
+    on_attach = function(client, bufnr)
+      ih.on_attach(client, bufnr)
+      vim.keymap.set('n', '<leader>ca', rust_tools.hover_actions.hover_actions, { buffer = bufnr })
+    end
+  }
 })
+-- END Rust
