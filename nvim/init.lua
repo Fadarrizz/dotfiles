35b93457724711f9df2266e20370d2668627748b
Fadarrizz <aukegeerts@duck.com>
2023-11-15 13:50:07 +0100

    Cleanuip

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,25 +3,10 @@ vim.g.mapleader = [[ ]]
 vim.g.localleader = [[,]]
 
 -- SKIP REMOTE PROVIDER LOADING
-vim.g.loaded_python3_provider = 0
+-- vim.g.loaded_python3_provider = 0
 vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
-
-
--- DISABLE UNWANTED PLUGINS
--- local disabled_built_ins = {
---     'netrw',
---     'netrwPlugin',
--- }
-
--- for i  = 1,1 do
---     vim.g['loaded_' .. disabled_built_ins[i]] = 1
--- end
-
--- function R(name)
---     require("plenary.reload").reload_module(name)
--- end
 
 -- IMPORTS
 require('fadarrizz.autocmd')
