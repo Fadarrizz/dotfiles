bf4853461da169c164c7a281b32b1a80effc67af
Fadarrizz <aukegeerts@duck.com>
2024-04-19 14:07:14 +0200

    Plugin cleanup

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -2,9 +2,6 @@ return {
   'nvim-telescope/telescope.nvim',
   dependencies = {
     'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons',
-    'nvim-telescope/telescope-live-grep-args.nvim',
-    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
   },
   keys = {
     { '<leader>ff', function() require('telescope.builtin').find_files() end, desc = 'Find files' },
