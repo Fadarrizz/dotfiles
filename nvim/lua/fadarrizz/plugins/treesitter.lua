b5426f54e5733b4625d958b241c4cb024bd39e9f
Fadarrizz <aukegeerts@duck.com>
2026-04-07 15:34:13 +0200

    Update Treesitter to new version

--- nvim/lua/fadarrizz/plugins/treesitter.lua
+++ nvim/lua/fadarrizz/plugins/treesitter.lua
@@ -1,71 +1,120 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        lazy = false,
-        opts = {
-            ensure_installed = {
-                "vimdoc", "php", "javascript", "c", "go", "lua", "rust", "jsdoc", "bash",
-            },
-            ignore_install = { "help", "sh", "text" },
-            auto_install = true,
-            highlight = { enable = true, additional_vim_regex_highlighting = false },
-            indent = { enable = true },
-            textobjects = {
-                select = {
-                    enable = true,
-                    lookahead = true,
-                    keymaps = {
-                        ['if'] = '@function.inner',
-                        ['af'] = '@function.outer',
-                        ['ic'] = '@class.inner',
-                        ['ac'] = '@class.outer',
-                        ['il'] = '@loop.inner',
-                        ['al'] = '@loop.outer',
-                        ['ia'] = '@parameter.inner',
-                        ['aa'] = '@parameter.outer',
-                    },
-                },
-            },
-            endwise = { -- 'RRethy/nvim-treesitter-endwise',
-                enable = true,
-            },
-        },
-        config = function(_, opts)
-            require('nvim-treesitter.configs').setup(opts)
+  {
+    "nvim-treesitter/nvim-treesitter",
+    branch = "main",
+    lazy = false,
+    build = ":TSUpdate",
+    config = function()
+      local ts = require("nvim-treesitter")
 
-            local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
+      ts.setup({
+        install_dir = vim.fn.stdpath("data") .. "/site",
+      })
 
-            -- Blade
-            parser_config.blade = {
-                install_info = {
-                    url = "https://github.com/EmranMR/tree-sitter-blade",
-                    files = { "src/parser.c" },
-                    branch = "main",
-                },
-                filetype = "blade"
-            }
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "TSUpdate",
+        callback = function()
+          local parsers = require("nvim-treesitter.parsers")
 
-            -- Klog
-            parser_config.klog = {
-                install_info = {
-                    url = "https://github.com/Ansimorph/tree-sitter-klog",
-                    files = { "src/parser.c", "src/scanner.c" },
-                    branch = "main",
-                    -- generate_requires_npm = false,
-                    -- requires_generate_from_grammar = false,
-                },
-                filetype = "klog",
-            }
+          parsers.blade = {
+            install_info = {
+              url = "https://github.com/EmranMR/tree-sitter-blade",
+              files = { "src/parser.c" },
+              branch = "main",
+            },
+          }
+
+          parsers.klog = {
+            install_info = {
+              url = "https://github.com/Ansimorph/tree-sitter-klog",
+              files = { "src/parser.c", "src/scanner.c" },
+              branch = "main",
+            },
+          }
         end,
-        dependencies = {
-            'nvim-treesitter/nvim-treesitter-textobjects',
-            'JoosepAlviste/nvim-ts-context-commentstring',
-            'windwp/nvim-ts-autotag',
-            'RRethy/nvim-treesitter-endwise',
-        },
+      })
+
+      ts.install({
+        "vimdoc",
+        "php",
+        "javascript",
+        "c",
+        "go",
+        "lua",
+        "rust",
+        "jsdoc",
+        "bash",
+      })
+
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "php", "javascript", "c", "go", "lua", "rust", "bash", "vimdoc", "blade", "klog" },
+        callback = function(args)
+          pcall(vim.treesitter.start, args.buf)
+        end,
+      })
+
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "php", "javascript", "c", "go", "lua", "rust", "bash", "blade", "klog" },
+        callback = function(args)
+          vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
+      })
+    end,
+    dependencies = {
+      { "nvim-treesitter/nvim-treesitter-textobjects", branch = "main" },
+      "JoosepAlviste/nvim-ts-context-commentstring",
+      "windwp/nvim-ts-autotag",
+      -- "RRethy/nvim-treesitter-endwise",
     },
-    {
-        "windwp/nvim-ts-autotag",
-    }
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    branch = "main",
+    config = function()
+      require("nvim-treesitter-textobjects").setup({
+        select = { lookahead = true },
+      })
+
+      local select = require("nvim-treesitter-textobjects.select")
+
+      vim.keymap.set({ "x", "o" }, "if", function()
+        select.select_textobject("@function.inner", "textobjects")
+      end)
+      vim.keymap.set({ "x", "o" }, "af", function()
+        select.select_textobject("@function.outer", "textobjects")
+      end)
+      vim.keymap.set({ "x", "o" }, "ic", function()
+        select.select_textobject("@class.inner", "textobjects")
+      end)
+      vim.keymap.set({ "x", "o" }, "ac", function()
+        select.select_textobject("@class.outer", "textobjects")
+      end)
+      vim.keymap.set({ "x", "o" }, "il", function()
+        select.select_textobject("@loop.inner", "textobjects")
+      end)
+      vim.keymap.set({ "x", "o" }, "al", function()
+        select.select_textobject("@loop.outer", "textobjects")
+      end)
+      vim.keymap.set({ "x", "o" }, "ia", function()
+        select.select_textobject("@parameter.inner", "textobjects")
+      end)
+      vim.keymap.set({ "x", "o" }, "aa", function()
+        select.select_textobject("@parameter.outer", "textobjects")
+      end)
+    end,
+  },
+
+  {
+    "windwp/nvim-ts-autotag",
+    config = function()
+      require("nvim-ts-autotag").setup({
+        opts = {
+          enable_close = true,
+          enable_rename = true,
+          enable_close_on_slash = false,
+        },
+      })
+    end,
+  },
 }
